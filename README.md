f0d516b143f6f33a62126a4d935ed43169cfa805
antraeus <antreus@gmail.com>
2015-04-04 16:40:51 -0700

    Failed constructor to master merge. The master was not updated via the pull request somehow, and a git pull from master ended up eraasing the local files committed under the constructor branch. This commit should be an appendation of the failed constructor merge and a redaction of a git pull from master without the constructor branch merging successfully.

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 ```
 Test your working knowledge of various programming languages and their uses!
 ```
-
+### Change Log v 1.32
+	```
+	* Organization of elements within guessing_game.js
+	* Comments on potential DOM manipulation sites within the program
+	* Added several instances where a function is repeated into the console.logs for better debugging
+	* Added new instances of dialogue, while simultaneously freshening and editing the old
+	* Cleaned up the 'Ranking' function-method of the constructor object 'Player' by calling it as apart of the Player property-variable 'Player.rank' at the end of the quiz.
+  * Added structure to the index.html, while incorporating a guessing_game.css for future styling.
+	```
 ### Version
-1.3
+1.32
 ### Installation
-Simply run index.html in your browser
+* Place all files in the same directory.
+* Simply run index.html in your browser
+* .. and presto!
 ### License
-
+Creative Commons
 ### Credits
-Landon, TeamTreeHouse
+Claudia, Landon, TeamTreeHouse
 
--- guessing_game.js
+++ guessing_game.js
@@ -1,88 +1,109 @@
-/*
-+===========================================+
+/*==========================================+
 | Version 1.3                               |
 | Added object constructor for new player   |
 | creation, which in turn tracks their      |
 | score as they progress through the game.  |
-|                                           |
-+-=========================================-+
+| __________________________________________+
 */
-//Setting the up the user name and scoreboard
-function Player (name, totalScore) {
-  this.name       = name;
-  this.totalScore = totalScore;
-  totalScore      = 0;
+
+
+
+function Player (name) {
+  this.name            = name;
+  this.totalScore      = 0;
+  this.totalQuestions  = 5;
+  this.rank            = 0; //Moved out of global scope
+
+  this.ranking         = function() {
+    console.log("Player Total Score: " + this.totalScore + " Player Questions: " + this.totalQuestions);
+    this.rank += (this.totalQuestions - this.totalScore);
+    console.log("RANK IN RANKING: " + this.rank);
   };
-  Player.ranking  = function() {
-    return this.totalScore - totalQuestions;
-  }
-};
-
-var totalScore     = 0;
-var totalQuestions = 5;
-var newPlayer1     = new Player(prompt('Desired player name'));
-
-/*
-var Player.name = function () {
-  var answer = prompt();
-  if (answer.toUpperCase === 'YES') {
-
-  }
 }
-*/
 
-document.write("Welcome, " + newPlayer1.name);
-//
-// Start: #1
-var answer1 = prompt("Name a programming language that's also a gem?");
+var newPlayer     = new Player(prompt('What name are you called?'));                  //DOM FUNCTION
+////////////////////////////////////////////////////////////////////////////////
+// Welcome the user and gauge the contents of their feelings in an awkward manner.
+document.write("Welcome, " + newPlayer.name);
+var playerFeelings = prompt("We\'ll be sucking a few minutes of your life away," + newPlayer.name + ". How do you feel?");               //DOM FUNCTION
+console.log(playerFeelings);
+document.write('<br><p>The user is feeling: ' + playerFeelings + '.<br>  Retabulating our pre-existing lexicon of available terms to match your present emotional state..</p>')
+////////////////////////////////////////////////////////////////////////////////
+// Start: Q#1
+var answer1 = prompt("Name a programming language that's also a gem?");               //DOM FUNCTION
 if (answer1.toUpperCase() === 'RUBY') {
-  Player.totalScore++;
+  newPlayer.totalScore++;
+  document.write('<p>(4) questions remaining!</p>');
 } else {
-  alert('That\'s wrong!');
+  alert('Shameful display! You have lost face with your Acuminous Lanague Proctor.'); //DOM FUNCTION
+  document.write('<p>(4) questions remaining!</p>');
 }
+console.log(newPlayer.totalScore + " score after 1st question");
 // End: #1
-
-// Start: #2
-var answer2 = prompt("Name a programming language that's also a reptile?")
+////////////////////////////////////////////////////////////////////////////////
+// Start: Q#2
+var answer2 = prompt("Name a programming language that's also a reptile?");           //DOM FUNCTION
 if (answer2.toUpperCase() === 'PYTHON'){
-  Player.totalScore++;
+  newPlayer.totalScore++;
+  document.write('<p>(3) questions remaining!</p>');
 } else {
-  alert('That\'s wrong!');
+  alert('Shameful display! You have lost face with your Acuminous Lanague Proctor.'); //DOM FUNCTION
+  document.write('<p>(3) questions remaining!</p>');
 }
+console.log(newPlayer.totalScore + " score after 2nd question");
 // End: #2
-
-// Start: #3
-var answer3 = prompt("What language do you use to style web pages?");
+////////////////////////////////////////////////////////////////////////////////
+// Start: Q#3
+var answer3 = prompt("What language do you use to style web pages?");                  //DOM FUNCTION
 
 if (answer3.toUpperCase() === 'CSS') {
-  Player.totalScore++;
+  newPlayer.totalScore++;
+  document.write('<p>(2) questions remaining!</p>');
 } else {
-  alert('That\'s wrong!');
+  alert('Shameful display! You have lost face with your Acuminous Language Proctor.'); //DOM FUNCTION
+  document.write('<p>(2) questions remaining!</p>');
 }
+console.log(newPlayer.totalScore + " score after 3rd question");
 // End: #3
-
-// Start: #4
-var answer4 = prompt("What language do you use to create structure in your web pages?");
+////////////////////////////////////////////////////////////////////////////////
+// Start: Q#4
+var answer4 = prompt("What language do you use to create structure in your web pages?"); //DOM FUNCTION
 
 if (answer4.toUpperCase() === 'HTML') {
-  Player.totalScore++;
+  newPlayer.totalScore++;
+  document.write('<p>Last question!</p>');
 } else {
-  alert('That\'s wrong!');
+  alert('Shameful display! You have lost face with your Acuminous Language Proctor.'); //DOM FUNCTION
+  document.write('<p>Last question!</p>');
 }
+console.log(newPlayer.totalScore + " score after 4th question");
 // End: #4
+////////////////////////////////////////////////////////////////////////////////
+// Start: Q#5
+var answer5 = prompt("What langauge do you use to create interactivity into your web pages?"); //DOM FUNCTION
 
-// Start: #5
-var answer5 = prompt("What langauge do you use to create interactivity into your web pages?");
 if (answer5.toUpperCase() === 'JAVASCRIPT') {
-  Player.totalScore++;
-} else {
-  alert('That\'s wrong!');
-}
-// End: #5
+  newPlayer.totalScore++;
 
-// Results of totalScore
-if (Player.totalScore >= 3){
-  alert('The world shudders with awe in the wake of your triumph!');
 } else {
-  alert('The world shudders in abject horror from your dalliance with ignorance.' + totalScore.Player + ' is not passing.')
+  alert('Shameful display! You have lost face with your Acuminous Language Proctor.'); //DOM FUNCTION
+}
+console.log(newPlayer.totalScore + " score after 5th question");
+// End: #5
+////////////////////////////////////////////////////////////////////////////////
+                            // Results of totalScore with rank sorting
+                            newPlayer.ranking();
+                            console.log(newPlayer.rank + " this is the rank.");
+////////////////////////////////////////////////////////////////////////////////
+if (newPlayer.rank == 0){
+  document.write('<p>The world shudders with awe in the wake of your triumph! </br>Bask in your glory: you\'ve been awarded a shiny gold star. Wow! <br> <img src="http://i.imgur.com/pSrrGzA.gif"> </p>');
+
+} else if (newPlayer.rank == 1){
+  document.write('Heralds proclaim your name, and we dub thee' + newPlayer.name + ', the Penultimate');
+
+} else if (newPlayer.rank == 2){
+  document.write('<p>Honorable attempt, but you\'ve passed by the hair of your chinny-chin-chin. <br> We seem to have run out of gold stars, sorry</p>');
+
+} else {
+  document.write('<p>The world shudders in abject horror from your dalliance with ignorance, ' + newPlayer.totalScore + ' is not passing. </br> Please address this malfeasance and try again.</p>');
 }
